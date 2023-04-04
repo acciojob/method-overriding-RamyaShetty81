84374388ed7b3056c0e2ef584f774e3886af5bfe
ramya <ramyarshetty81@gmail.com>
2023-04-04 23:51:57 +0530

    meth

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,17 @@ package com.driver;
 
 public class Main {
 
+    public static class A {
+
+        String meth()
+        {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static class B extends A {
+
+    }
     public static void main(String[] args) {
         B obj = new B();
         obj.meth();
