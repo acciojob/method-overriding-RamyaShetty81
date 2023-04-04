b7ea0bc7c8c8bd261597c5a44d42a8f68b3fbb26
ramya <ramyarshetty81@gmail.com>
2023-04-05 00:08:13 +0530

    invoke A

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,18 +4,24 @@ public class Main {
 
     public static class A {
 
-        public static String meth()
+        public  String meth()
         {
-            return "Method is overridden in Extendend class B";
+            return "Invoking method from A";
         }
     }
 
     public static class B extends A {
-
+        public  String meth()
+        {
+            return "Method is overridden in Extendend class B";
+        }
     }
     public static void main(String[] args) {
         B obj = new B();
         obj.meth();
+
+        B obj1 = new B();
+        obj1.meth();
     }
   
 }
